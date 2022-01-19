667839e8d099811547ae0507c4b241f7efd41451
dev1000123 <85182710+dev1000123@users.noreply.github.com>
2022-01-19 13:21:38 +0530

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "can":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . f f f . . . . . 
+. . . . . . . f f 2 f f f . . . 
+. . . . . . f f e e 2 2 f f . . 
+. . . . . f f 2 2 e e 2 2 f . . 
+. . . . . f 5 2 2 2 e e 2 f . . 
+. . . . f 2 5 5 e e 2 e e f . . 
+. . . . f 2 2 e e 5 5 2 f f . . 
+. . . f f e 2 2 e 2 5 5 f . . . 
+. . . f 2 e e 2 e 2 2 f f . . . 
+. . . f e e e e 5 e f f . . . . 
+. . . f f 2 2 e 2 5 f . . . . . 
+. . . . f f f 2 e 2 f . . . . . 
+. . . . . . f f f f f . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
